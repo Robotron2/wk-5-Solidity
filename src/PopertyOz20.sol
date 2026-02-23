1d3c3775045bbdc4baa61246ff8662a5c917c3b2
Theophilus <theophilusadesola002@gmail.com>
2026-02-23 19:29:44 +0100

    feat: property utility token implemented via openzeppelin II

--- src/PopertyOz20.sol
+++ src/PopertyOz20.sol
@@ -1,18 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+// Compatible with OpenZeppelin Contracts ^5.5.0
+pragma solidity ^0.8.27;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract RobotronToken is ERC20, AccessControl {
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+contract RobotronToken is ERC20, Ownable, ERC20Permit {
+    constructor(address initialOwner) ERC20("Robotron", "RBTK") Ownable(initialOwner) ERC20Permit("Robotron") {}
 
-    constructor() ERC20("RobotronToken", "RTK") {
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(MINTER_ROLE, msg.sender);
-    }
-
-    function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) {
+    function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 }
